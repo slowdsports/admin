c98b62763c9b7a7a0437103c6f691d064dfb9412
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-02-29 23:50:35 -0600

    Update sofa.php

--- inc/componentes/back/sofa.php
+++ inc/componentes/back/sofa.php
@@ -10,40 +10,6 @@ if (!isset($_POST['filtrarLiga'])) {
 $i = 0;
 
 
-
-// Motor
-
-if ($apiLeague == "f1") {
-    // Games Info
-    $apiUrl = "https://api.sofascore.com/api/v1/stage/206456/substages";
-    $data = file_get_contents($apiUrl);
-    $jsonData = json_decode($data, true);
-
-    // Verificamos si hay datos válidos obtenidos de la API
-    if (isset($jsonData['stages']) && is_array($jsonData['stages'])) {
-        foreach ($jsonData['stages'] as $stage) {
-            // Aquí necesitas usar $stage en lugar de $event
-            $i++;
-            $id = $stage['id'];
-            $tipo = $stage['uniqueStage']['category']['name'];
-            $etapa = $stage['description'];
-            $fecha = date('Y-m-d H:i:s', $stage['startDateTimestamp']);
-            $ciudad = $stage['stageParent']['description'];
-            // Realizar operaciones con $country si es necesario
-            $game_insert = "INSERT INTO `motor`(`id`, `tipo`, `etapa`, `fecha_hora`, `ciudad`) VALUES ($id, $tipo, $etapa, $fecha, '$ciudad')";
-            mysqli_query($conn, $game_insert);
-        }
-        if ($game_insert) {
-            echo "Se agregaron " . $i . " carreras de " . $tipo;
-        } else {
-            echo "Error al agregar las carreras: " . mysqli_error($conn);
-        }
-    } else {
-        echo "No se encontraron datos válidos para esa liga.";
-    }
-}
-
-
 // Deporte General
 
 
