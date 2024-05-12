7fa1e15a57982059f47276d486faca1c5ceea0c5
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-12 00:50:27 -0600

    Update sofa.php

--- inc/componentes/back/sofa.php
+++ inc/componentes/back/sofa.php
@@ -95,7 +95,7 @@ if ($apiLeague):
                 $game_insert = "INSERT INTO `partidos`(`id`, `local`, `visitante`, `liga`, `fecha_hora`, `tipo`, `canal1`, `canal2`, `canal3`, `canal4`, `canal5`) VALUES ($game_id, $home_id, $away_id, $tournament_id, '$date', '$sport', '32', '33', '34', '35', '36')";
             break;
             // Liga Costa Rica (FUTV)
-            case 11535:
+            case 11535: case 11542:
                 $game_insert = "INSERT INTO `partidos`(`id`, `local`, `visitante`, `liga`, `fecha_hora`, `tipo`, `canal1`) VALUES ($game_id, $home_id, $away_id, $tournament_id, '$date', '$sport', '332')";
             break;
             // Primera Uruguay (VTV, VTV+)
