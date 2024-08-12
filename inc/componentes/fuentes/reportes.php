99a3673544e363e4ce18be41cc743eeb2a175669
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-08-12 11:32:09 -0600

    Create reportes.php

---
+++ inc/componentes/fuentes/reportes.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <a href="?p=fuentes&agregar" class="btn btn-outline-primary">+ Agregar</a>
+                <a href="?p=fuentes&reportes" class="btn btn-outline-warning"> Ver Reportes</a>
+                <hr>
+                <table id="basic-datatable" class="table table-striped dt-responsive nowrap w-100">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Categoria</th>
+                            <th>Fecha</th>
+                            <th>Acción</th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+                        <?php
+                        while ($result = mysqli_fetch_array($reportes)):
+                            ?>
+                            <tr id="fuente-<?= $result['fuenteId'] ?>">
+                                <td class="table-user">
+                                    <img src="../assets/img/canales/<?= $result['canalImg']; ?>.png"
+                                        alt="table-user" class="me-2 rounded-circle">
+                                        <?= $result['fuenteNombre']; ?> (<?= $result['paisNombre']; ?>)
+                                </td>
+                                <td>
+                                    <?= $result['categoriaNombre']; ?>
+                                </td>
+                                <td>
+                                    <?= $result['ultimaFecha']; ?>
+                                </td>
+                                <td>
+                                    <div class="btn-group mb-2">
+                                        <a href="?p=fuentes&editar=<?= $result['fuenteId'] ?>"
+                                            class="btn btn-outline-primary">Editar</a>
+                                        <button type="button" class="btn btn-outline-danger eliminarFuente"
+                                        data-id="<?= $result['fuenteId'] ?>" onclick="eliminarFuente()">Borrar</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+
+            </div> <!-- end card body-->
+        </div> <!-- end card -->
+    </div><!-- end col-->
+</div>
+<!-- end row -->
